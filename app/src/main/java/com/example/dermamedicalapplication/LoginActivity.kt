1181d3a60e3ef91ec5d90eaa14c1af3971d7bf0b
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-05-07 15:30:27 +0700

    [add] handleClickLogin and handleClickRegister in LoginActivity

--- app/src/main/java/com/example/dermamedicalapplication/LoginActivity.kt
+++ app/src/main/java/com/example/dermamedicalapplication/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.example.dermamedicalapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dermamedicalapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // handle login click
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        // handle register click
+        binding.noAccountTv.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
     }
 }
