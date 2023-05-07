bbdf1eb2f32bfa236f89f60391a805a926846d60
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-05-07 21:17:14 +0700

    [add] handleBackEventClick for backBtn in Register

--- app/src/main/java/com/example/dermamedicalapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/dermamedicalapplication/RegisterActivity.kt
@@ -2,10 +2,21 @@ package com.example.dermamedicalapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.dermamedicalapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // handle back click
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
