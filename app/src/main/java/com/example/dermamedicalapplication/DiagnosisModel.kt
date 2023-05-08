073b73a38162ce2e0991b19e0a4e7e3c1f4a4ec7
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-05-08 20:29:24 +0700

    [add] loadDiagnosis in HistoryLayout

---
+++ app/src/main/java/com/example/dermamedicalapplication/DiagnosisModel.kt
@@ -0,0 +1,12 @@
+package com.example.dermamedicalapplication
+
+class DiagnosisModel {
+    var id:String = ""
+    var name:String = ""
+    var image:String = ""
+    var timestamp:String = ""
+    var uid:String = ""
+    var disease_id:String = ""
+    var description:String = ""
+    var status:String = ""
+}
--- app/src/main/java/com/example/dermamedicalapplication/HistoryActivity.kt
+++ app/src/main/java/com/example/dermamedicalapplication/HistoryActivity.kt
@@ -2,10 +2,39 @@ package com.example.dermamedicalapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dermamedicalapplication.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHistoryBinding
+
+    private lateinit var diagnosisArrayList: ArrayList<DiagnosisModel>
+
+    private lateinit var diagnosisAdapter: DiagnosisAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+        }
+
+        loadDiagnosises()
+    }
+
+    private fun loadDiagnosises() {
+
+        diagnosisArrayList = ArrayList()
+
+        repeat(10) {
+            diagnosisArrayList.add(DiagnosisModel())
+        }
+
+        diagnosisAdapter = DiagnosisAdapter(this@HistoryActivity, diagnosisArrayList)
+
+        binding.diagnosisRv.adapter = diagnosisAdapter
+
     }
 }
