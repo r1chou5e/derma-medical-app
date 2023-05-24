8f70f5acc450e5fa6457fd43cee3c0c6a082fa2d
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-05-25 01:06:34 +0700

    [add] dateOfBirth picker in questionLayout

--- app/src/main/java/com/example/dermamedicalapplication/QuestionActivity.kt
+++ app/src/main/java/com/example/dermamedicalapplication/QuestionActivity.kt
@@ -1,19 +1,31 @@
 package com.example.dermamedicalapplication
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.DatePicker
 import com.example.dermamedicalapplication.databinding.ActivityQuestionBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class QuestionActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityQuestionBinding
+    private lateinit var binding: ActivityQuestionBinding
+
+    private lateinit var calendar: Calendar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        calendar = Calendar.getInstance()
+        val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        binding.dobEt.setText(dateFormatter.format(calendar.time))
 
         binding.bottomNavigationView.selectedItemId = R.id.diagnose
 
@@ -30,5 +42,21 @@ class QuestionActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+    fun showDatePickerDialog(view: View) {
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                calendar.set(year, month, dayOfMonth)
+                val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                binding.dobEt.setText(dateFormatter.format(calendar.time))
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+        datePickerDialog.show()
     }
 }
