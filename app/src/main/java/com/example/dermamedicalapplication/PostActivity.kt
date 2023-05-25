1c7724c6e263aaa19e7a61f0d0c7a90632ec6119
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-05-26 01:42:58 +0700

    [add] uploadImage for postLayout

--- app/src/main/java/com/example/dermamedicalapplication/PostActivity.kt
+++ app/src/main/java/com/example/dermamedicalapplication/PostActivity.kt
@@ -1,12 +1,25 @@
 package com.example.dermamedicalapplication
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.dermamedicalapplication.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPostBinding
+
+    private var imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +28,10 @@ class PostActivity : AppCompatActivity() {
 
         binding.cancelBtn.setOnClickListener {
             onBackPressed()
+        }
+
+        binding.imageBtn.setOnClickListener {
+            ThumbnailImageViewFragment().show(supportFragmentManager, "newThumbnail")
         }
 
     }
