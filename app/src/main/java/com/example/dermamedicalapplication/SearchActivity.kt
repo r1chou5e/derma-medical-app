a88c084862077d75df13c031596183e8621af1f9
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-02 12:30:20 +0700

    [add] load postTitle on Search

--- app/src/main/java/com/example/dermamedicalapplication/SearchActivity.kt
+++ app/src/main/java/com/example/dermamedicalapplication/SearchActivity.kt
@@ -2,13 +2,22 @@ package com.example.dermamedicalapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Sampler.Value
 import android.widget.ArrayAdapter
 import android.widget.SearchView
 import com.example.dermamedicalapplication.databinding.ActivitySearchBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SearchActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivitySearchBinding
+    private lateinit var binding: ActivitySearchBinding
+
+    private lateinit var postArrayList: ArrayList<PostModel>
+
+    private lateinit var postTitleArray: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +28,44 @@ class SearchActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        val posts = arrayOf("Vì sao phải uống nước", "Top 5 bài thuốc dân gian trị nấm ngoài da", "Mật ong có tác dụng gì cho làn da của bạn ?", "Thức khuya có làm ảnh hưởng đến sức khỏe ?")
+        loadSearchResult()
+
+    }
+
+    private fun loadSearchResult() {
+
+        postArrayList = ArrayList()
+        postTitleArray = ArrayList()
+
+        val ref = FirebaseDatabase.getInstance().getReference("Post")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                postArrayList.clear()
+                for (ds in snapshot.children) {
+                    // get data as model
+                    val post = ds.getValue(PostModel::class.java)
+                    // add to arrayList
+                    postArrayList.add(post!!)
+
+                    if (post.title.length > 47) {
+                        val subString = post.title.substring(0,44)
+                        postTitleArray.add("$subString...")
+                    }
+                    else {
+                        postTitleArray.add(post.title)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
         val postAdapter: ArrayAdapter<String> = ArrayAdapter (
             this, android.R.layout.simple_list_item_1,
-            posts
+            postTitleArray
         )
 
         binding.searchResultLv.adapter = postAdapter
@@ -30,7 +73,7 @@ class SearchActivity : AppCompatActivity() {
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(p0: String?): Boolean {
                 binding.searchView.clearFocus()
-                if (posts.contains(p0)) {
+                if (postTitleArray.contains(p0)) {
                     postAdapter.filter.filter(p0)
                 }
 
