33adf564a578c112dec7bc4a6bfdb7e2193289a2
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-03 12:43:32 +0700

    [add] loadUserInfo and scrollView for postDetail

--- app/src/main/java/com/example/dermamedicalapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/dermamedicalapplication/ProfileActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dermamedicalapplication.databinding.ActivityProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -38,11 +42,37 @@ class ProfileActivity : AppCompatActivity() {
             true
         }
 
+        loadUserInfo()
+
         binding.logoutRl.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
 
+    }
+
+    private fun loadUserInfo() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null) {
+            binding.fullnameTv.text = "Chưa đăng nhập"
+        }
+        else {
+            val ref = FirebaseDatabase.getInstance().getReference("User").child(firebaseUser.uid)
+            ref.addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val user = snapshot.getValue(UserModel::class.java)
+                        binding.fullnameTv.text = user?.fullname.toString()
+                        binding.emailTv.text = user?.email.toString()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
     }
 }
